468610ead8cf7e7c2aa97eee3dc0a9ec8ab58b53
Steph Khaguli <24970338+skha10@users.noreply.github.com>
2025-11-24 02:02:15 +0300

    fix duplicte app function issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,4 @@
 // App.jsx - Main application component
-function App() {
-  console.log('🔥 DEPLOYMENT TEST: Email + Floating Emojis SHOULD be here');
-  // ... rest of your code
-}
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
